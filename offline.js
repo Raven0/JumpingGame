832ecd82d12969893b676cb273a12876ea12dcf5
Ismail Rohaga <azh.zhafir@gmail.com>
2017-02-07 21:17:46 +0700

    Initial Commit
    Beres buat submission project 2 meren

---
+++ offline.js
@@ -0,0 +1,24 @@
+﻿{
+	"version": 1486469954,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"images/background.png",
+		"images/background2.png",
+		"images/background3.png",
+		"images/background4.png",
+		"images/groundsprite-sheet0.png",
+		"images/player-sheet0.png",
+		"images/btnleft-sheet0.png",
+		"images/btnright-sheet0.png",
+		"images/btnstart-sheet0.png",
+		"images/btnrestart-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
